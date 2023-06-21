3e1ec6d86f5b28a9b3b8c57d18316fba77985e1b
Mostafa <Mostafagholipore@gmail.com>
2023-06-22 00:47:02 +0330

    create BookGroups

--- src/main/java/org/example/entity/BookGroups.java
+++ src/main/java/org/example/entity/BookGroups.java
@@ -1,16 +1,15 @@
 package org.example.entity;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 import org.example.base.entity.BaseEntity;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Setter
@@ -20,9 +19,19 @@ import java.util.Set;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class BookGroups extends BaseEntity {
     String title;
-    @OneToMany(mappedBy = "name",orphanRemoval = true,cascade = CascadeType.ALL)
-    Set<Book> books=new HashSet<>();
+    @OneToMany(mappedBy = "bookGroups",cascade = CascadeType.ALL)
+    List<Book> books=new ArrayList<>();
 
+    public BookGroups(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "BookGroups{" +
+                "title='" + title + '\'' +
+                '}';
+    }
 }
 
 
