b0fb34553ed56184c116a630ba5e375dc5dfd497
Mostafa <Mostafagholipore@gmail.com>
2023-06-22 00:47:17 +0330

    create BookGroupsRepository

--- src/main/java/org/example/repository/bookGroups/BookGroupsRepository.java
+++ src/main/java/org/example/repository/bookGroups/BookGroupsRepository.java
@@ -1,7 +1,11 @@
 package org.example.repository.bookGroups;
 
 import org.example.base.repository.BaseRepository;
+import org.example.entity.Book;
 import org.example.entity.BookGroups;
 
+import java.util.List;
+
 public interface BookGroupsRepository extends BaseRepository<BookGroups> {
+    public List<BookGroups> findAtLeast();
 }
--- src/main/java/org/example/repository/bookGroups/BookGroupsRepositoryImpl.java
+++ src/main/java/org/example/repository/bookGroups/BookGroupsRepositoryImpl.java
@@ -2,10 +2,25 @@ package org.example.repository.bookGroups;
 
 import org.example.base.repository.BaseRepositoryImpl;
 import org.example.entity.BookGroups;
+import org.example.util.Hibernate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import java.util.List;
 
 public class BookGroupsRepositoryImpl extends BaseRepositoryImpl<BookGroups> implements BookGroupsRepository {
     @Override
     public Class getClassName() {
         return BookGroups.class;
+    }
+
+    @Override
+    public List<BookGroups> findAtLeast() {
+        EntityManager entityManager= Hibernate.getEntityManager();
+        EntityTransaction transaction=entityManager.getTransaction();
+        transaction.begin();
+        String jql="SELECT m from BookGroups m where m.books.size>0";
+        List<BookGroups> resultList = entityManager.createQuery(jql, BookGroups.class).getResultList();
+        return resultList;
     }
 }
