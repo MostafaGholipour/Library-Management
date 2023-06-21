096fd5f5c7c8d43412c26b6800b32c1dc8049845
Mostafa <Mostafagholipore@gmail.com>
2023-06-22 00:47:27 +0330

    create BookGroupsService

--- src/main/java/org/example/service/bookGroups/BookGroupsService.java
+++ src/main/java/org/example/service/bookGroups/BookGroupsService.java
@@ -3,5 +3,8 @@ package org.example.service.bookGroups;
 import org.example.base.service.BaseService;
 import org.example.entity.BookGroups;
 
+import java.util.List;
+
 public interface BookGroupsService extends BaseService<BookGroups> {
+    public List<BookGroups> findAtLeast();
 }
--- src/main/java/org/example/service/bookGroups/BookGroupsServiceImpl.java
+++ src/main/java/org/example/service/bookGroups/BookGroupsServiceImpl.java
@@ -4,10 +4,17 @@ import org.example.base.service.BaseServiceImpl;
 import org.example.entity.BookGroups;
 import org.example.repository.bookGroups.BookGroupsRepositoryImpl;
 
+import java.util.List;
+
 public class BookGroupsServiceImpl extends BaseServiceImpl<BookGroups, BookGroupsRepositoryImpl> implements BookGroupsService {
     BookGroupsRepositoryImpl repository1;
     public BookGroupsServiceImpl(BookGroupsRepositoryImpl repository) {
         super(repository);
         repository1=repository;
+    }
+
+    @Override
+    public List<BookGroups> findAtLeast() {
+        return repository1.findAtLeast();
     }
 }
