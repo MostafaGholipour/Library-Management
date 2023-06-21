2f65b03a700ebfff7454fceb538a1f42e3b8ba23
Mostafa <Mostafagholipore@gmail.com>
2023-06-22 00:49:59 +0330

    create BookService

--- src/main/java/org/example/service/book/BookService.java
+++ src/main/java/org/example/service/book/BookService.java
@@ -4,4 +4,5 @@ import org.example.base.service.BaseService;
 import org.example.entity.Book;
 
 public interface BookService extends BaseService<Book> {
+    public void changeBookGroups(Long idBook ,Long idBookGroup );
 }
--- src/main/java/org/example/service/book/BookServiceImpl.java
+++ src/main/java/org/example/service/book/BookServiceImpl.java
@@ -2,12 +2,24 @@ package org.example.service.book;
 
 import org.example.base.service.BaseServiceImpl;
 import org.example.entity.Book;
+import org.example.entity.BookGroups;
 import org.example.repository.book.BookRepositoryImpl;
+import org.example.repository.bookGroups.BookGroupsRepositoryImpl;
+import org.example.service.bookGroups.BookGroupsServiceImpl;
 
 public class BookServiceImpl extends BaseServiceImpl<Book, BookRepositoryImpl> implements BookService {
     BookRepositoryImpl repository1;
     public BookServiceImpl(BookRepositoryImpl repository) {
         super(repository);
         repository1=repository;
+    }
+
+    @Override
+    public void changeBookGroups(Long idBook, Long idBookGroup) {
+        BookGroupsServiceImpl bookGroupsService=new BookGroupsServiceImpl(new BookGroupsRepositoryImpl());
+        Book book=repository1.loadById(idBook);
+        BookGroups bookGroups = bookGroupsService.loadById(idBookGroup);
+        book.setBookGroups(bookGroups);
+        repository1.update(book);
     }
 }
